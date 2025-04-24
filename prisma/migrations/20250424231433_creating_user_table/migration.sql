83c175f77e1fc75daf7f1ef70398b151b970da04
Jean Lincoln <jeanlincol@gmail.com>
2025-04-24 20:26:15 -0300

    Add Prisma client and Prisma dependencies to package.json and pnpm-lock.yaml

---
+++ prisma/migrations/20250424231433_creating_user_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(63) NOT NULL,
+    "email" VARCHAR(127) NOT NULL,
+    "password" VARCHAR(127) NOT NULL,
+    "createdAt" TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
